6878c4f88af5fff6e12a266af8f2da2ec247ee72
DAESEEK <69086490+DAESEEK@users.noreply.github.com>
2021-10-24 15:29:41 +0700

    Add files via upload

---
+++ Exam1.java
@@ -0,0 +1,16 @@
+package bagicStudy;
+
+public class Exam1 {
+	
+	public static void main(String[]arg) {
+		
+		for(int i =2; i<10; i++) {
+			for(int j=2; j<10; j++) {
+				System.out.printf(i+"*"+j+"="+i*j+" ");		
+			}
+			System.out.println("");
+		}
+	
+	}
+
+}
